a10aacb38d43d024d84bc1d98883d3a2b6343d64
OLIKO marghishvili <oliko.margishvili@gmail.com>
2020-09-30 22:04:31 +0400

    Add project files.

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using OdeToFood.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>
+            {
+                new Restaurant{Id=1,Name="Pizza",Cuisine=CuisineType.Italian },
+                new Restaurant{Id=2,Name="Sushi",Cuisine=CuisineType.Georgian }
+            };
+        }
+
+        public List<Restaurant> Restaurants => restaurants;
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(n => n.Name);
+        }
+    }
+}
