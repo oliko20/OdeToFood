1ff5f9ab9683d9ae6f0a3a1ae2ea58b1978ab533
OLIKO marghishvili <oliko.margishvili@gmail.com>
2020-10-01 22:33:26 +0400

    add restaurants controller

--- OdeToFood.Web/Global.asax.cs
+++ OdeToFood.Web/Global.asax.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -13,7 +13,8 @@ namespace OdeToFood.Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters); 
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             ContainerConfig.RegisterContainer();
