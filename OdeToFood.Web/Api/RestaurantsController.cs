b270453307142249df5043e6f3ca6d9032152104
OLIKO marghishvili <oliko.margishvili@gmail.com>
2020-10-01 22:45:02 +0400

    add controller to configuration

--- OdeToFood.Web/Api/RestaurantsController.cs
+++ OdeToFood.Web/Api/RestaurantsController.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using OdeToFood.Data.Models;
+using OdeToFood.Data.Services;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace OdeToFood.Web.Api
 {
     public class RestaurantsController : ApiController
     {
+        public IRestaurantData RestaurantsData { get; }
+
+        public RestaurantsController(IRestaurantData restaurantsData)
+        {
+            RestaurantsData = restaurantsData;
+        }
+        public IEnumerable<Restaurant> Get()
+        { var model = RestaurantsData.GetAll();
+            return model;
+        }
     }
 }
--- OdeToFood.Web/App_Start/ContainerConfig.cs
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -1,13 +1,15 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
 using OdeToFood.Data.Services;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace OdeToFood.Web
 {
     public class ContainerConfig
     {
-        internal static void RegisterContainer()
+        internal static void RegisterContainer(HttpConfiguration configuration)
         {
             var builder = new ContainerBuilder();
             builder.RegisterControllers(typeof(MvcApplication).Assembly);
@@ -17,6 +19,7 @@ namespace OdeToFood.Web
 
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);     
         }
     }
 }
